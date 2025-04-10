1c834875c610f8956299649a29d27c216338a06f
racevedo12 <ronny.acv2@hotmail.com>
2025-04-09 23:01:05 -0400

    Refactoring code to include classes in just a single file and added new methods to the window class

---
+++ graphics.py
@@ -0,0 +1,51 @@
+from tkinter import Tk, BOTH, Canvas
+
+
+class Window():
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+        self.__root = Tk()
+        self.__root.title("Maze Solver")
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+        self.__canvas = Canvas(self.__root, bg="white", height=self.height, width=self.width)
+        self.__canvas.pack(fill=BOTH, expand=1)
+
+        self.is_running = False
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.is_running = True
+        while self.is_running:
+            self.redraw()
+        print("Window closed.")
+
+    def close(self):
+        self.is_running = False
+
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+
+
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        
+
+class Line():
+    def __init__(self, point_1, point_2):
+        self.point_1 = point_1
+        self.point_2 = point_2
+
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(
+            self.point_1.x, self.point_1.y, self.point_2.x, self.point_2.y, fill=fill_color, width=2
+        )
+
+
