970c4833af12b8dc6187228bb2164363c193b99a
racevedo12 <ronny.acv2@hotmail.com>
2025-04-10 15:57:22 -0400

    Created Maze class

--- main.py
+++ main.py
@@ -1,29 +1,22 @@
 from graphics import Window
-from cell import Cell
+from maze import Maze
 
 
 def main():
-    win = Window(800, 600)
+    num_rows = 12
+    num_cols = 16
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
+    win = Window(screen_x, screen_y)
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
     
-    cell = Cell(win)
-    cell.has_left_wall = False
-    cell.draw(50, 50, 100, 100)
-
-    cell = Cell(win)
-    cell.has_right_wall = False
-    cell.draw(125, 125, 200, 200)
-
-    cell = Cell(win)
-    cell.has_bottom_wall = False
-    cell.draw(225, 225, 250, 250)
-
-    cell = Cell(win)
-    cell.has_top_wall = False
-    cell.draw(300, 300, 500, 500)
+    
 
     win.wait_for_close()
     
-
 
 if __name__ == "__main__":
     main()
